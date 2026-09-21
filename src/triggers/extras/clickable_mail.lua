b63a6f659428709f06e58ae0c7d0c9807264ee64
DarthVenator228 <huntertjohnson9@gmail.com>
2026-09-20 22:13:17 -0400

    small clickable mail bug fix

--- src/triggers/extras/clickable_mail.lua
+++ src/triggers/extras/clickable_mail.lua
@@ -1,16 +1,21 @@
 if not lotj.settings.clickable_mail then return end
 
+lotj.settings.clickable_mail_sender = matches.sender
+
 lotj.settings.clickable_mailRegexTrigger = tempRegexTrigger(
   [[^(?<id>\d+)\s*\|\s*(?<sender>.+?)\s*\|\s*(?<recipient>.+?)\s*\|\s(?<subject>.+?)$|^(?<empty>)$]],
   function()
     if matches.empty then
-      -- killTrigger(lotj.settings.clickable_mailRegexTrigger)
       return false
     end
     local targetNumber = matches.id
     selectString(matches.subject:trim(), 1)
     setUnderline(true)
-    setLink([[send("note read ]] .. targetNumber .. [[")]], "note read ".. targetNumber)
+    if lotj.settings.clickable_mail_sender == "SENDER" then
+      setLink([[send("mail read ]] .. targetNumber .. [[")]], "mail read ".. targetNumber)
+    else
+      setLink([[send("note read ]] .. targetNumber .. [[")]], "note read ".. targetNumber)
+    end
     resetFormat()
     return true
   end,
